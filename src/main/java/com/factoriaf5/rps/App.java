be9478bdfa74d4bc58cbbcd2f2a4202a5275a6aa
Brusmel Gil <Brusmel@outlook.es>
2024-02-09 09:40:02 +0100

    ultimo intento

--- src/main/java/com/factoriaf5/rps/App.java
+++ src/main/java/com/factoriaf5/rps/App.java
@@ -1,7 +1,5 @@
 package com.factoriaf5.rps;
 
-import com.factoriaf5.rps.application.Game;
-
 public class App 
 {
     public static void main( String[] args ){
--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -1,8 +1,5 @@
 package com.factoriaf5.rps.application;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 import com.factoriaf5.rps.models.Player;
