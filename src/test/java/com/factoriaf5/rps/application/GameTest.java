f8c1af0dec10b55a0e18204a016ccace71752ec6
Brusmel Gil <Brusmel@outlook.es>
2024-02-09 09:36:08 +0100

    ultimo intento

--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -1,13 +1,27 @@
 package com.factoriaf5.rps.application;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.factoriaf5.rps.models.Player;
+
 public class GameTest {
     
     @Test
-    public void justASimpleTest() {
-        assertTrue(true);
+    public void TestSofiaWins() {
+        
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        Game game = new Game("Sofia", "Camila", "Scissors wins",
+        "Rock Wins",
+        "Scissors Wins", "paper Wins",
+        "Lizard Wins", "Spock Wins", 
+        "Scissors Wins", "Lizard Wins", "Paper Wins", 
+        "Spock Wins", "Rock Wins");
+
+
     }
 }
